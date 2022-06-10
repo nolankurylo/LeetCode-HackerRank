21c8c74663d7336cb7136045325e60648ad3879a
Nolan Kurylo <44009838+nolankurylo@users.noreply.github.com>
2022-06-10 11:32:33 -0700

    Update LongestConsecutiveSequence.py

--- Arrays&Hashing/LongestConsecutiveSequence.py
+++ Arrays&Hashing/LongestConsecutiveSequence.py
@@ -2,19 +2,25 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         
         nums.sort()
-        longest_sequence = 0
-        curr_sequence = 
+        longest_sequence = 1
+        curr_sequence = 1
         
+        if len(nums) == 0:
+            return 0
         
-        for i in range(1, len(nums)+1):
+        for i in range(1, len(nums)):
             prev_num = nums[i-1]
             curr_num = nums[i]
             
+            if curr_num == prev_num:
+                continue
+            
             if curr_num - 1 == prev_num:
-                curr_sequence +=1
+                curr_sequence += 1
             else:
                 if curr_sequence > longest_sequence:
                     longest_sequence = curr_sequence
+                curr_sequence = 1
             
                 
             
